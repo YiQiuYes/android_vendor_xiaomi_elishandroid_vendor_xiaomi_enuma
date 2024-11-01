f677aac7527094188ba1776b6739cec7bdae19cc
YiQiuYes <binzixiang@163.com>
2024-11-01 13:41:29 +0800

    Update Blobs
    Change-Id: I6346e787a13af9ec6e5411d00cdcc30826025d11

--- sm8250-common/sm8250-common-vendor.mk
+++ sm8250-common/sm8250-common-vendor.mk
@@ -640,6 +640,8 @@ PRODUCT_PACKAGES += \
     libEGL_adreno_libEGL_adreno_symlink64 \
     libGLESv2_adreno_libGLESv2_adreno_symlink64 \
     libq3dtools_adreno_libq3dtools_adreno_symlink64
+
+ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/sm8250-common/proprietary/vendor/etc/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
     vendor/xiaomi/sm8250-common/proprietary/vendor/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
@@ -670,3 +672,4 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-service-qti \
     mlipayd@1.1 \
     slim_daemon
+endif
